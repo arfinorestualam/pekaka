a43a628a156f0532289d86feefd51eb885d8a5c8
arfinorestualam <arfino.alam@gmail.com>
2021-10-28 23:49:40 +0700

    update

--- app/src/main/java/com/example/peluangkerja/SplashScreen.java
+++ app/src/main/java/com/example/peluangkerja/SplashScreen.java
@@ -1,25 +1,86 @@
 package com.example.peluangkerja;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SplashScreen extends AppCompatActivity {
-    final private static int SPLASH_TIME_OUT = 3500;
+    final private static int SPLASH_TIME_OUT = 100;
+    private static final int PERMISSION_ALL = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        setPermissionForM();
 
+    }
+    private void displaySplashScreen(){
         Handler handler = new Handler();
         Runnable runnable = () -> {
-            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            Intent intent = new Intent(SplashScreen.this, loginActivity.class);
             startActivity(intent);
             finish();
         };
         handler.postDelayed(runnable, SPLASH_TIME_OUT);
     }
-}
+
+    private void setPermissionForM() {
+        String[] PERMISSIONS = {
+                //Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_PHONE_STATE};
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        } else {displaySplashScreen();}
+    }
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Map<String, Integer> perms = new HashMap<String, Integer>();
+        // Initial
+       // perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+        perms.put(Manifest.permission.ACCESS_COARSE_LOCATION, PackageManager.PERMISSION_GRANTED);
+        perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+        perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
+        perms.put(Manifest.permission.READ_PHONE_STATE, PackageManager.PERMISSION_GRANTED);
+        for (int i = 0; i < permissions.length; i++)
+            perms.put(permissions[i], grantResults[i]);
+        if (//perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+              /*  &&*/ perms.get(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                && perms.get(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            // All Permissions Granted
+            displaySplashScreen();
+        } else {
+            // Permission Denied
+            Toast.makeText(SplashScreen.this, "Some Permission is Denied", Toast.LENGTH_SHORT)
+                    .show();
+        }
+}}
